0ea6301b3908ca30c78e75adbf27b45d169c9764
albert <albert@hyperplane.hu>
2022-12-01 19:00:30 +0100

    Remove webdriver_manager import.

--- run.py
+++ run.py
@@ -10,7 +10,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from pushnotifier import PushNotifier as pn
 
 warnings.filterwarnings('ignore')
